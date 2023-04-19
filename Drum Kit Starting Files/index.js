a6c8ca266d5f87a89096cf636ba5b2ad76631f9a
ericksondsantos <ericksondsantos@gmail.com>
2023-04-19 15:56:08 +0800

    apr19

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,6 +1,7 @@
 var buttonCount = document.querySelectorAll(".drum").length;
 for(var i=0; i<buttonCount; i++){
-    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+    var clickedButton = document.querySelectorAll(".drum")[i];
+    clickedButton.addEventListener("click", function(){
         makeSound(this.innerHTML);
         buttonAnimation(this.innerHTML);
     });
