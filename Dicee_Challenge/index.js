42b50851e1a153ed96047d5092667ca0f2230479
ericksondsantos <ericksondsantos@gmail.com>
2023-04-29 02:17:15 +0800

    apr29

--- Dicee_Challenge/index.js
+++ Dicee_Challenge/index.js
@@ -1,16 +1,25 @@
-var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+$(document).click(function() {
+    rollDice();
+});
 
-document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
-document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+$(document).keydown(function() {
+    rollDice();
+});
 
-if(randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML = "Player 1 Wins!";
+function rollDice() {
+    var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+    var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+    document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+    if(randomNumber1 > randomNumber2) {
+        document.querySelector("#winner").innerHTML = "Player 1 Wins!";
+    }
+    else if(randomNumber1 < randomNumber2) {
+        document.querySelector("#winner").innerHTML = "Player 2 Wins!";
+    }
+    else {
+        document.querySelector("#winner").innerHTML = "Draw!";
+    }
 }
-else if(randomNumber1 < randomNumber2) {
-    document.querySelector("h1").innerHTML = "Player 2 Wins!";
-}
-else {
-    document.querySelector("h1").innerHTML = "Draw!";
-}
+
     
