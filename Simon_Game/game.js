4bc8e7937e54f54a9ff78315b0ddda1f7298e3f8
ericksondsantos <ericksondsantos@gmail.com>
2023-04-30 02:03:28 +0800

    apr30

--- Simon_Game/game.js
+++ Simon_Game/game.js
@@ -18,7 +18,7 @@ $(document).click(function() {
 
 function nextSequence() {
     started = true;
-    $("#level-title").text("Level: " + level);
+    $("#level-title").text("{ level: " + level + " }");
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
