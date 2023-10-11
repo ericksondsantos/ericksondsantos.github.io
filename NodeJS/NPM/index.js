5719ab81a3846e0c8f70d7b28bf558079359b326
ericksondsantos <ericksondsantos@gmail.com>
2023-10-12 03:43:29 +0800

    10/12

---
+++ NodeJS/NPM/index.js
@@ -0,0 +1,9 @@
+// import generateName from "sillyname";
+// var sillyName = generateName();
+
+// console.log(`My name is ${sillyName}.`)
+
+import superheroes from "superheroes";
+const superheroName = superheroes.random();
+
+console.log(`I am ${superheroName}.`)
