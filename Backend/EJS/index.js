2cee088b5b892b87f80768260c486b153740487e
Erickson Santos <ericksondsantos@gmail.com>
2023-12-02 20:10:12 +0800

    dec2

--- Backend/EJS/index.js
+++ Backend/EJS/index.js
@@ -4,20 +4,16 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  const today = new Date();
-  const day = today.getDay();
-  var week = "a weekday";
-  var time = "it's time to work hard";
-  if (day == 0 || day == 6) {
-    week = "the weekend";
-    time = "it's time to have some fun";
-  }
-  res.render("/views/index.ejs", {
-    dayType: week,
-    advice: time,
-  });
+    const today = new Date();
+    const day = today.getDay();
+    if (day === 0 || day === 6){
+        res.render("index.ejs", {dayType: "weekend", advise: "it's time to play hard"});
+    }
+    else{
+        res.render("index.ejs", {dayType: "weekday", advise: "it's time to work hard"});
+    }
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}.`);
+    console.log(`Listening on port ${port}`);
 });
