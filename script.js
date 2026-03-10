430d167bc64ec095df76db37dab1373603373eb9
Erickson Santos <ericksondsantos@gmail.com>
2026-03-11 04:29:06 +0800

    Mar11

---
+++ script.js
@@ -0,0 +1,18 @@
+const observerOptions = {
+  root: null,
+  threshold: 0.2
+};
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('is-visible');
+    } else {
+      entry.target.classList.remove('is-visible');
+    }
+  });
+}, observerOptions);
+
+document.querySelectorAll('.reveal').forEach(div => {
+  observer.observe(div);
+});
