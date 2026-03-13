c1c67dffe7bfd69f24751767a003af6e909b3611
Erickson Santos <ericksondsantos@gmail.com>
2026-03-14 00:14:47 +0800

    Mar13

--- script.js
+++ script.js
@@ -16,15 +16,6 @@ leftZone.addEventListener('click', () => {
 let touchStartX = 0;
 let touchEndX = 0;
 
-wrapper.addEventListener('touchstart', e => {
-  touchStartX = e.changedTouches[0].screenX;
-});
-
-wrapper.addEventListener('touchend', e => {
-  touchEndX = e.changedTouches[0].screenX;
-  handleSwipe();
-});
-
 wrapper.addEventListener('scroll', () => {
   const containerWidth = wrapper.offsetWidth;
   const scrollLeft = wrapper.scrollLeft;
@@ -32,18 +23,7 @@ wrapper.addEventListener('scroll', () => {
   slides.forEach((slide, index) => {
     const slideOffset = slide.offsetLeft;
     const distanceFromCenter = Math.abs(scrollLeft - slideOffset);
-    
     let opacity = 1 - (distanceFromCenter / containerWidth);
-    
     slide.style.opacity = Math.max(0, Math.min(1, opacity));
   });
 });
-
-function handleSwipe() {
-  const threshold = 50;
-  if (touchStartX - touchEndX > threshold) {
-    wrapper.scrollLeft += scrollAmount;
-  } else if (touchEndX - touchStartX > threshold) {
-    wrapper.scrollLeft -= scrollAmount;
-  }
-}
